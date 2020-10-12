36283e3affb5cb9215f0236408cec2effcdd5985
JenifferGenoves <jenifer.genoves03@gmail.com>
2020-10-11 23:11:44 -0300

    Revert merge -->Position 405e4bf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,99 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route
+} from "react-router-dom";
+import Home from './views/home';
+import AboutUs from './views/aboutUs';
+import Projects from './views/projects';
+import Campaigns from './views/campaigns';
+import Collaborators from './views/collaborators';
+import Contact from './views/contact';
+import Donate from './views/donate';
+import Transparency from './views/transparency';
+import './App.css';
+import UrbanGardens from './views/urbanGardens';
+import Foods from './views/foods'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route
+          exact path="/"
+          render={() => <Home />}
+        />
+        <Route
+          exact path="/aboutUs"
+          render={() => <AboutUs />}
+        />
+        <Route
+          exact path="/projects"
+          render={() => <Projects />}
+        />
+        <Route
+          exact path="/campaings"
+          render={() => <Campaigns />}
+        />
+        <Route
+          exact path="/collaborators"
+          render={() => <Collaborators />}
+        />
+        <Route
+          exact path="/transparency"
+          render={() => <Transparency />}
+        />
+        <Route
+          exact path="/contact"
+          render={() => <Contact />}
+        />
+        <Route
+          exact path="/donate"
+          render={() => <Donate />}
+        />
+        <Route
+          exact path="/donate"
+          render={() => <Donate />}
+        />
+        <Route
+          exact path="/donate/urbangarden"
+          render={() => <UrbanGardens />}
+        />
+        <Route
+          exact path="/donate/foods"
+          render={() => <Foods />}
+        />
+      </Switch>
+    </Router >
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -24,3 +119,4 @@ function App() {
 }
 
 export default App;
+ */
